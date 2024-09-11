3ed03a3a8d408d776243ac14af96a1def2a3792f
Sadaqat Ali <sadaqatali890@gmail.com>
2024-09-11 21:30:29 +0530

    add agile model and solid principle questions

---
+++ laravel/09_agile_model.php
@@ -0,0 +1,13 @@
+<?php
+
+// todo : Agile Methodology
+
+// ! The Agile methodology is a project management approach that involves breaking the project into phases and emphasizes continuous collaboration and improvement. Teams follow a cycle of planning, executing, and evaluating.
+// ! Agile methodology has multiple iteration to develop the complete software.
+// ! It follows the iterative as well as incremental approach for developing the projects.
+// ! It focuses on delivering smaller pieces of work regularly instead of one big launch.
+// ! It has much client interaction.
+// ! Plan -> Design -> Develop -> Test -> Deploy -> Review
+
+// * Water fall model => requirement -> design -> coding -> testing -> deployment -> maintenance
+// * SDLC, follows a systematic approach with sequential stages like planning, design, development, testing, and deployment, suitable for projects of any size.
---
+++ laravel/10_solid_principle.php
@@ -0,0 +1,9 @@
+<?php
+
+// SOLID is a set of five design principles that developers use when creating object-oriented software.
+
+// 1. Single Responsibility Principle (SRP)
+// 2. Open-Closed Principle (OCP)
+// 3. Liskov Substitution Principle (LSP)
+// 4. Interface Segregation Principle (ISP)
+// 5. Dependency Inversion Principle (DIP)
