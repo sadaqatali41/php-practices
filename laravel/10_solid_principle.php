1365f735ae8c8437a1a3da419b9cba50429e0d68
Sadaqat Ali <sadaqatali890@gmail.com>
2024-09-16 21:29:07 +0530

    add solid principle and PSR standard

--- laravel/10_solid_principle.php
+++ laravel/10_solid_principle.php
@@ -2,8 +2,8 @@
 
 // SOLID is a set of five design principles that developers use when creating object-oriented software.
 
-// 1. Single Responsibility Principle (SRP)
-// 2. Open-Closed Principle (OCP)
-// 3. Liskov Substitution Principle (LSP)
-// 4. Interface Segregation Principle (ISP)
-// 5. Dependency Inversion Principle (DIP)
+// 1. Single Responsibility Principle (SRP) ==> A class should have only one reason to change, which means every class should have a single responsibility or single job or single purpose.
+// 2. Open-Closed Principle (OCP)           ==> Software entities (classes, modules, functions, etc.) should be open for extension, but closed for modification. (should be able to extends class behavior, without modifying it.) 
+// 3. Liskov Substitution Principle (LSP)   ==> Derived or child classes must be substitutable for their base or parent classes. (ex. rectangle is parent class and square is child class).
+// 4. Interface Segregation Principle (ISP) ==> do not force any client to implement an interface which is irrelevant to them.
+// 5. Dependency Inversion Principle (DIP)  ==> High-level modules should not depend on low-level modules. Both should depend on abstractions.
---
+++ laravel/11_psr.php
@@ -0,0 +1,3 @@
+<?php
+
+// PSR stands for PHP Standard Recommendation. It’s a set of guidelines for PHP coding standards and practices, developed by the PHP Framework Interop Group. There are several PSRs covering various aspects like autoloading (PSR-4), coding style (PSR-12), and logging (PSR-3).
