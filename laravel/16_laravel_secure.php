d6bee998067df3a0157f682f50d7d9ae0c9d3172
Sadaqat Ali <sadaqatali890@gmail.com>
2025-04-16 22:08:43 +0530

    add laravel security concern

---
+++ laravel/16_laravel_secure.php
@@ -0,0 +1,13 @@
+<?php
+
+// ! To secure a Laravel application, we need to follow the below things: 👇
+
+// 01. Keep laravel updated
+// 02. Communication over HTTPS
+// 03. Authentication and Authorization
+// 04. Input Validation and Sanitization
+// 05. Cross-Site Scripting (XSS) Protection
+// 06. Cross-Site Request Forgery (CSRF) Protection
+// 07. SQL Injection Prevention
+// 08. Sensitive Data Security
+// 09. Rate Limiting
