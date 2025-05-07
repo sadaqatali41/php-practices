319287435390450b8348c776b0650fcf5ae52639
Sadaqat Ali <sadaqatali890@gmail.com>
2025-05-07 14:44:57 +0530

    add load balancer question and modified psr question

---
+++ 31_load_balancer.php
@@ -0,0 +1,6 @@
+<?php
+
+// A load balancer is a device or software that distributes network traffic across multiple servers, 
+// also known as backends or application servers.
+// Load balancers improve application performance and increase availability.
+// They help to ensure that no single server becomes overwhelmed with too much traffic.
